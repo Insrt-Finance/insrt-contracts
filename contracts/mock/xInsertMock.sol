ea158e668284c7f38a285f8e37a0969e6513dc83
NouDaimon <peric.iason@gmail.com>
2022-03-18 19:36:20 +0200

    xInsert

---
+++ contracts/mock/xInsertMock.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import { IERC20 } from '@solidstate/contracts/token/ERC20/IERC20.sol';
+import { xInsert } from '../xInsert.sol';
+
+contract xInsertMock is xInsert {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals,
+        IERC20 insertToken
+    ) xInsert(name, symbol, decimals, insertToken) {}
+
+    function __mint(address recipient, uint256 amount) external {
+        _mint(recipient, amount);
+    }
+
+    function __burn(address recipient, uint256 amount) external {
+        _burn(recipient, amount);
+    }
+}
---
+++ test/xInsert.ts
@@ -0,0 +1,60 @@
+import {
+  InsertMock,
+  InsertMock__factory,
+  XInsertMock,
+  XInsertMock__factory,
+} from '../typechain-types';
+import { describeBehaviorOfERC20 } from '@solidstate/spec';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { ethers } from 'hardhat';
+import { BigNumber } from 'ethers';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+describe('xInsert', () => {
+  let instance: XInsertMock;
+  let insertToken: InsertMock;
+  let deployer: SignerWithAddress;
+
+  const name: string = 'ERC20MetadataStorage.name';
+  const symbol: string = 'ERC20MetadataStorage.symbol';
+  const decimals: BigNumber = BigNumber.from('18');
+
+  beforeEach(async () => {
+    [deployer] = await ethers.getSigners();
+
+    insertToken = await new InsertMock__factory(deployer).deploy(
+      name,
+      symbol,
+      decimals,
+    );
+
+    instance = await new XInsertMock__factory(deployer).deploy(
+      name,
+      symbol,
+      decimals,
+      insertToken.address,
+    );
+  });
+
+  describeBehaviorOfERC20({
+    deploy: async () => instance,
+    mint: async (recipient, amount) =>
+      await instance['__mint(address,uint256)'](recipient, amount),
+    burn: async (recipient, amount) =>
+      await instance['__burn(address,uint256)'](recipient, amount),
+    name,
+    symbol,
+    decimals,
+    supply: ethers.constants.Zero,
+  });
+
+  describe('#deposit(uint256)', () => {
+    it('mints tokens at 1:1 if supply is 0', async () => {});
+
+    it('mints tokens with correct rate', async () => {});
+  });
+
+  describe('#withdraw(uint256)', () => {
+    it('sends correct corresponding amount of Insert tokens', async () => {});
+  });
+});
