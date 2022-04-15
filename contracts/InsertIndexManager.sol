e3aa2b224d479af35e753c455ef2643a40267d41
Nick Barry <itsnickbarry@protonmail.ch>
2022-04-15 17:05:35 -0500

    remove InsertIndexManager

--- contracts/InsertIndexManager.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
-pragma experimental ABIEncoderV2;
-
-import { IVault } from '@balancer-labs/v2-vault/contracts/interfaces/IVault.sol';
-
-abstract contract InsertIndexManager is IVault {}
--- test/Balancer.ts
+++ test/Balancer.ts
@@ -1,15 +1,17 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { ethers } from 'hardhat';
-import IVault from '../artifacts/@balancer-labs/v2-vault/contracts/interfaces/IVault.sol/IVault.json';
+import { IVault, IVault__factory } from '../typechain-types';
 
 describe.only('Balancer', () => {
   //taken from: https://github.com/balancer-labs/balancer-v2-monorepo/blob/master/pkg/deployments/tasks/20210418-vault/output/arbitrum.json
   const balancerVaultAddress = '0xBA12222222228d8Ba445958a75a0704d566BF2C8';
 
+  let balancerVault: IVault;
+
   before(async () => {
-    const balancerVault = await ethers.getContractAt(
-      IVault.abi,
+    balancerVault = IVault__factory.connect(
       balancerVaultAddress,
+      ethers.provider,
     );
 
     console.log(
@@ -20,10 +22,6 @@ describe.only('Balancer', () => {
 
   describe('test', () => {
     it('tests', async () => {
-      const balancerVault = await ethers.getContractAt(
-        IVault.abi,
-        balancerVaultAddress,
-      );
       console.log(
         await balancerVault.getPool(ethers.utils.formatBytes32String('test')),
       );
