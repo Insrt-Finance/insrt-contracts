3f317bb77934956e1e278860a9a225c9d321b969
Nick Barry <itsnickbarry@protonmail.ch>
2022-04-21 19:55:34 -0500

    add missing IInvestmentPoolFactory interface

---
+++ contracts/index/IInvestmentPoolFactory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import { IERC20 } from '@solidstate/contracts/token/ERC20/IERC20.sol';
+
+/**
+ * @title Balancer InvestmentPoolFactory interface
+ */
+interface IInvestmentPoolFactory {
+    function create(
+        string memory name,
+        string memory symbol,
+        IERC20[] memory tokens,
+        uint256[] memory weights,
+        address[] memory assetManagers,
+        uint256 swapFeePercentage,
+        address owner,
+        bool swapEnabledOnStart
+    ) external returns (address);
+}
