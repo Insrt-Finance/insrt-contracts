bf1528c830663107bd5122d668e2346e86870be7
NouDaimon <peric.iason@gmail.com>
2022-05-25 19:47:02 +0300

    missing commit

--- contracts/index/IndexView.sol
+++ contracts/index/IndexView.sol
@@ -2,29 +2,28 @@
 
 pragma solidity ^0.8.0;
 
+import { IIndexView } from './IIndexView.sol';
 import { IndexInternal } from './IndexInternal.sol';
 import { IndexStorage } from './IndexStorage.sol';
 import { IBalancerHelpers } from '../balancer/IBalancerHelpers.sol';
 import { IInvestmentPool } from '../balancer/IInvestmentPool.sol';
 import { IVault, IAsset } from '../balancer/IVault.sol';
 
-contract IndexView is IndexInternal {
+/**
+ * @title Infra Index view functions
+ * @dev deployed standalone and referenced by IndexProxy
+ */
+contract IndexView is IndexInternal, IIndexView {
     using IndexStorage for IndexStorage.Layout;
 
     constructor(address balancerVault, address balancerHelpers)
         IndexInternal(balancerVault, balancerHelpers)
     {}
 
-    //TODO: Query other deposits
-    //TODO: Modify name
     /**
-     * @notice function to return the BPT given for a certain amount of underlying Balancer InvesmentPool tokens
-     * @param amounts an array comprised of the amount of each underlying token
-     * @param minBPTAmountOut the minimum amount of BPT accepted as a return
-     * @return bptOut the BPT returned
-     * @return amountsIn the amounts to be taken in by Balancer InvestmentPool for the BPT returned
+     * @inheritdoc IIndexView
      */
-    function queryUserDepositExactIn(
+    function queryUserDepositExactInForAnyOut(
         uint256[] memory amounts,
         uint256 minBPTAmountOut
     ) external returns (uint256 bptOut, uint256[] memory amountsIn) {
@@ -34,6 +33,54 @@ contract IndexView is IndexInternal {
             amounts,
             minBPTAmountOut
         );
+
+        (bptOut, amountsIn) = IBalancerHelpers(BALANCER_HELPERS).queryJoin(
+            l.poolId,
+            address(this),
+            address(this),
+            request
+        );
+    }
+
+    /**
+     * @inheritdoc IIndexView
+     */
+    function queryUserDepositSingleForExactOut(
+        uint256[] memory amounts,
+        uint256 bptAmountOut,
+        uint256 tokenIndex
+    ) external returns (uint256 bptOut, uint256[] memory amountsIn) {
+        IndexStorage.Layout storage l = IndexStorage.layout();
+        IVault.JoinPoolRequest
+            memory request = _constructJoinSingleForExactRequest(
+                l,
+                amounts,
+                bptAmountOut,
+                tokenIndex
+            );
+
+        (bptOut, amountsIn) = IBalancerHelpers(BALANCER_HELPERS).queryJoin(
+            l.poolId,
+            address(this),
+            address(this),
+            request
+        );
+    }
+
+    /**
+     * @inheritdoc IIndexView
+     */
+    function queryUserDepositAllForExactOut(
+        uint256[] memory amounts,
+        uint256 bptAmountOut
+    ) external returns (uint256 bptOut, uint256[] memory amountsIn) {
+        IndexStorage.Layout storage l = IndexStorage.layout();
+        IVault.JoinPoolRequest
+            memory request = _constructJoinAllForExactRequest(
+                l,
+                amounts,
+                bptAmountOut
+            );
 
         (bptOut, amountsIn) = IBalancerHelpers(BALANCER_HELPERS).queryJoin(
             l.poolId,
