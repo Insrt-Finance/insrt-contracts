c0f4b0c0456eca3c54f3026dd823e772b06caf19
Nick Barry <itsnickbarry@protonmail.ch>
2022-06-06 03:15:43 +0300

    set placehodler metadata for base Balancer pool token

--- contracts/index/IndexProxy.sol
+++ contracts/index/IndexProxy.sol
@@ -8,6 +8,7 @@ import { Proxy } from '@solidstate/contracts/proxy/Proxy.sol';
 import { IDiamondReadable } from '@solidstate/contracts/proxy/diamond/readable/IDiamondReadable.sol';
 import { IERC20 } from '@solidstate/contracts/token/ERC20/IERC20.sol';
 import { ERC4626BaseStorage } from '@solidstate/contracts/token/ERC4626/base/ERC4626BaseStorage.sol';
+import { AddressUtils } from '@solidstate/contracts/utils/AddressUtils.sol';
 
 import { IInvestmentPoolFactory } from '../balancer/IInvestmentPoolFactory.sol';
 import { IInvestmentPool } from '../balancer/IInvestmentPool.sol';
@@ -17,6 +18,8 @@ import { IndexStorage } from './IndexStorage.sol';
  * @title Upgradeable proxy with externally controlled Index implementation
  */
 contract IndexProxy is Proxy {
+    using AddressUtils for address;
+
     address private immutable INDEX_DIAMOND;
 
     constructor(
@@ -29,13 +32,16 @@ contract IndexProxy is Proxy {
     ) {
         INDEX_DIAMOND = indexDiamond;
 
+        string memory metadata = string(
+            abi.encodePacked('INSRT-INDEX-', address(this).toString())
+        );
+
         // deploy Balancer pool
 
         address balancerPool = IInvestmentPoolFactory(investmentPoolFactory)
             .create(
-                // TODO: metadata naming conventions?
-                'TODO: name',
-                'TODO: symbol',
+                metadata,
+                metadata,
                 tokens,
                 weights,
                 0.02 ether, // swapFeePercentage: 2%
