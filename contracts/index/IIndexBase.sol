7a7748e9b5848e65294a1e9dac7dd947a9ac25d9
Nick Barry <itsnickbarry@protonmail.ch>
2022-06-07 12:50:40 +0300

    fix IIndexBase interface inheritance

--- contracts/index/IIndexBase.sol
+++ contracts/index/IIndexBase.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import { IERC20Metadata } from '@solidstate/contracts/token/ERC20/metadata/IERC20Metadata.sol';
-import { IERC4626 } from '@solidstate/contracts/token/ERC4626/IERC4626.sol';
+import { ISolidStateERC4626 } from '@solidstate/contracts/token/ERC4626/ISolidStateERC4626.sol';
 
-interface IIndexBase is IERC4626, IERC20Metadata {}
+interface IIndexBase is ISolidStateERC4626 {}
