fb47d7a4a39a8c43c26ca416159d603f19dcd726
NouDaimon <peric.iason@gmail.com>
2022-06-13 19:01:01 +0300

    mock token script rework, interface to be more informative

--- scripts/deployMockTokens.ts
+++ scripts/deployMockTokens.ts
@@ -3,26 +3,71 @@ import {
   SolidStateERC20Mock,
   SolidStateERC20Mock__factory,
 } from '../typechain-types';
-import { createDir, createFile, CoreAddresses } from './utils/utils';
+import { createDir, createFile, MockToken } from './utils/utils';
+
+async function tokensToMockTokens(
+  tokens: SolidStateERC20Mock[],
+): Promise<MockToken[]> {
+  const MockTokens: MockToken[] = [];
+
+  for (let i = 0; i < tokens.length; i++) {
+    let MockToken = {
+      name: await tokens[i].name(),
+      symbol: await tokens[i].symbol(),
+      address: tokens[i].address,
+    };
+    MockTokens.push(MockToken);
+  }
+
+  return MockTokens;
+}
 
 async function main() {
   const ethers = hre.ethers;
   const [deployer] = await ethers.getSigners();
-  const tokens: string[] = [];
+  let tokens: MockToken[];
 
   const dirPath = `data`;
   const network = `arbitrum`;
   createDir(`/${dirPath}/${network}`);
 
-  const tokenOne: SolidStateERC20Mock = await new SolidStateERC20Mock__factory(
+  const mockNFTX: SolidStateERC20Mock = await new SolidStateERC20Mock__factory(
     deployer,
-  ).deploy();
-  const tokenTwo: SolidStateERC20Mock = await new SolidStateERC20Mock__factory(
+  ).deploy('MockNFTX', 'MNFTX');
+  const mockApe: SolidStateERC20Mock = await new SolidStateERC20Mock__factory(
     deployer,
-  ).deploy();
+  ).deploy('MockApe', 'MAPE');
+  const mockSand: SolidStateERC20Mock = await new SolidStateERC20Mock__factory(
+    deployer,
+  ).deploy('MockSand', 'MSand');
+  const mockJpegd: SolidStateERC20Mock = await new SolidStateERC20Mock__factory(
+    deployer,
+  ).deploy('MockJpegd', 'MJPEGD');
+  const mockAaveGotchi: SolidStateERC20Mock =
+    await new SolidStateERC20Mock__factory(deployer).deploy(
+      'MockAaveGotchi',
+      'MAVG',
+    );
+  const mockAudio: SolidStateERC20Mock = await new SolidStateERC20Mock__factory(
+    deployer,
+  ).deploy('MockAudio', 'MAUD');
+  const mockETH: SolidStateERC20Mock = await new SolidStateERC20Mock__factory(
+    deployer,
+  ).deploy('MockETH', 'METH');
+  const mockRare: SolidStateERC20Mock = await new SolidStateERC20Mock__factory(
+    deployer,
+  ).deploy('MockRare', 'MRARE');
 
-  tokens.push(tokenOne.address);
-  tokens.push(tokenTwo.address);
+  tokens = await tokensToMockTokens([
+    mockNFTX,
+    mockApe,
+    mockSand,
+    mockJpegd,
+    mockAaveGotchi,
+    mockAudio,
+    mockETH,
+    mockRare,
+  ]);
 
   createFile(`${dirPath}/${network}/mockTokens.json`, JSON.stringify(tokens));
 }
@@ -30,6 +75,7 @@ async function main() {
 main()
   .then(() => process.exit(0))
   .catch((error) => {
+    console.log('An unexpected error has occurred: ');
     console.error(error);
     process.exit(1);
   });
