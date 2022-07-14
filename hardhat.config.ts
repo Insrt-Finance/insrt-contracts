e9f73ddf4c893bd2a89caa0a71c72c4302d84fe5
Nick Barry <itsnickbarry@protonmail.ch>
2022-07-14 21:05:10 +0300

    set solidity version to 0.8.15

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,7 +27,7 @@ export default {
   solidity: {
     compilers: [
       {
-        version: '0.8.11',
+        version: '0.8.15',
         settings: {
           optimizer: {
             enabled: true,
