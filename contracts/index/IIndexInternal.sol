f86ab15bf4beee658c5eb88f6153371f213dcd7f
NouDaimon <peric.iason@gmail.com>
2022-07-16 22:47:53 +0300

    add StreamingFeePaid event

---
+++ contracts/index/IIndexInternal.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+/**
+ * @title interface needed for Index Facet events
+ */
+interface IIndexInternal {
+    event StreamingFeePaid(uint256 amount);
+}
