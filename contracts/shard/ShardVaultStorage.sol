b5a4e2c21f21aa10864e93b894ce403bbb2515f8
NouDaimon <peric.iason@gmail.com>
2022-09-29 14:57:51 +0300

    add internal deposit function

--- contracts/shard/ShardVaultStorage.sol
+++ contracts/shard/ShardVaultStorage.sol
@@ -2,14 +2,19 @@
 
 pragma solidity ^0.8.0;
 
+import { EnumerableSet } from '@solidstate/contracts/utils/EnumerableSet.sol';
+
 library ShardVaultStorage {
     struct Layout {
         uint256 id;
         uint256 shardSize;
+        uint256 maxCapital;
+        uint256 totalShards;
         address collection;
         bool invested;
         bool divested;
         mapping(address => uint256) owedShards;
+        EnumerableSet.AddressSet depositors;
     }
 
     bytes32 internal constant STORAGE_SLOT =
--- contracts/shard/ShartVaultInternal.sol
+++ contracts/shard/ShartVaultInternal.sol
@@ -2,9 +2,12 @@
 
 pragma solidity ^0.8.0;
 
+import { AddressUtils } from '@solidstate/contracts/utils/AddressUtils.sol';
+import { EnumerableSet } from '@solidstate/contracts/utils/EnumerableSet.sol';
 import { ERC1155BaseInternal } from '@solidstate/contracts/token/ERC1155/base/ERC1155BaseInternal.sol';
 import { ERC1155MetadataStorage } from '@solidstate/contracts/token/ERC1155/metadata/ERC1155MetadataStorage.sol';
 
+import { Errors } from './Errors.sol';
 import { ShardVaultStorage } from './ShardVaultStorage.sol';
 
 /**
@@ -12,5 +15,24 @@ import { ShardVaultStorage } from './ShardVaultStorage.sol';
  * @dev inherited by all Shard Vault implementation contracts
  */
 abstract contract ShardVaultInternal is ERC1155BaseInternal {
+    using AddressUtils for address payable;
+    using EnumerableSet for EnumerableSet.AddressSet;
 
+    function _deposit() internal {
+        ShardVaultStorage.Layout storage l = ShardVaultStorage.layout();
+
+        uint256 amount = msg.value;
+
+        if (amount % l.shardSize != 0) {
+            revert Errors.InvalidDepositAmount();
+        }
+        if (address(this).balance + amount > l.maxCapital) {
+            revert Errors.MaxCapitalExceeded();
+        }
+
+        uint256 shards = amount / l.shardSize;
+        l.owedShards[msg.sender] += shards;
+        l.totalShards += shards;
+        l.depositors.add(msg.sender);
+    }
 }
