b64f33bab4640fd18b1d114623222e96644e0ae8
NouDaimon <peric.iason@gmail.com>
2022-09-30 20:08:50 +0300

    jpegd vault interface

---
+++ contracts/jpegd/INFTVault.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+/**
+ * @title Interface to jpeg'd NFTVault Contracts
+ * @dev https://github.com/jpegd/core/blob/main/contracts/vaults/NFTVault.sol
+ */
+interface INFTVault {
+    /// @notice Allows users to open positions and borrow using an NFT
+    /// @dev emits a {Borrowed} event
+    /// @param _nftIndex The index of the NFT to be used as collateral
+    /// @param _amount The amount of PUSD to be borrowed. Note that the user will receive less than the amount requested,
+    /// the borrow fee and insurance automatically get removed from the amount borrowed
+    /// @param _useInsurance Whether to open an insured position. In case the position has already been opened previously,
+    /// this parameter needs to match the previous insurance mode. To change insurance mode, a user needs to close and reopen the position
+    function borrow(
+        uint256 _nftIndex,
+        uint256 _amount,
+        bool _useInsurance
+    ) external;
+
+    /// @param _nftIndex The NFT to return the value of
+    /// @return The value in USD of the NFT at index `_nftIndex`, with 18 decimals.
+    function getNFTValueUSD(uint256 _nftIndex) external view returns (uint256);
+}
