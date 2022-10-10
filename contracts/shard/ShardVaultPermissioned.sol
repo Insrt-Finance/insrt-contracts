24ae10259a57a50c466a73954154b38ad12a7b83
NouDaimon <peric.iason@gmail.com>
2022-10-10 21:00:12 +0300

    fill ShardVaultPermissioned facet

--- contracts/shard/ShardVaultPermissioned.sol
+++ contracts/shard/ShardVaultPermissioned.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import { ShardVaultInternal } from './ShardVaultInternal.sol';
+import { ShardVaultStorage } from './ShardVaultStorage.sol';
 
 contract ShardVaultPermissioned is ShardVaultInternal {
     constructor(
@@ -26,4 +27,43 @@ contract ShardVaultPermissioned is ShardVaultInternal {
             yieldFeeBP
         )
     {}
+
+    function purchasePunk(uint256 punkId) external payable {
+        _onlyProtocolOwner();
+        _purchasePunk(punkId);
+    }
+
+    function collateralizePunk(uint256 punkId, bool insure) external {
+        _onlyProtocolOwner();
+        _collateralizePunk(ShardVaultStorage.layout(), punkId, insure);
+    }
+
+    function stake(uint256 amount, uint256 minCurveLP) external {
+        _onlyProtocolOwner();
+        _stake(ShardVaultStorage.layout(), amount, minCurveLP);
+    }
+
+    function investPunk(
+        uint256 punkId,
+        uint256 minCurveLP,
+        bool insure
+    ) external {
+        _onlyProtocolOwner();
+        _investPunk(ShardVaultStorage.layout(), punkId, minCurveLP, insure);
+    }
+
+    function setFundraiseFee(uint256 feeBP) external {
+        _onlyProtocolOwner();
+        _setFundraiseFee(feeBP);
+    }
+
+    function setSalesFee(uint256 feeBP) external {
+        _onlyProtocolOwner();
+        _setSalesFee(feeBP);
+    }
+
+    function setYieldFee(uint256 feeBP) external {
+        _onlyProtocolOwner();
+        _setYieldFee(feeBP);
+    }
 }
