748c7301c0a6b686d1cfaf41f91700b3862f3d32
NouDaimon <peric.iason@gmail.com>
2022-10-11 11:28:12 +0300

    shard vault interface

---
+++ contracts/shard/IShardVault.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import { IShardVaultIO } from './IShardVaultIO.sol';
+
+/**
+ * @title complete ShardVault interface
+ */
+interface IShardVault is IShardVaultIO {
+
+}
