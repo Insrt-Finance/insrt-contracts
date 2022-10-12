2668a368df6b7661f0f3126d2098b26853048977
NouDaimon <peric.iason@gmail.com>
2022-10-12 16:31:40 +0300

    add ShardVaultDiamond contract

---
+++ contracts/shard/ShardVaultDiamond.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import { SolidStateDiamond } from '@solidstate/contracts/proxy/diamond/SolidStateDiamond.sol';
+
+/**
+ * @title Diamond proxy used as centrally controlled ShardVault implementation
+ * @dev deployed standalone and passed to ShardVaultManager constructor
+ */
+contract IndexDiamond is SolidStateDiamond {
+
+}
